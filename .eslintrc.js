aeb9e043ac72a576d37363213a4a5cb66d9f3bf0
Sanyam Arya <er.sanyam.arya@gmail.com>
2022-09-14 11:51:46 +0200

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,12 @@ module.exports = {
   "rules": {
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'no-use-before-define': 'off',
   },
 }
